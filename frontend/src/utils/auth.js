bfb6a541661d0a5f5a11d22006e0c1c11f0ab8ab
Alex Shevelkov <sasha.shevelkov0@yandex.ru>
2023-10-26 23:59:10 +0300

    fix: api

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,5 +1,5 @@
-export const BASE_URL = "http://localhost:3000";
-// export const BASE_URL = "https://api.fox.nomoredomainsrocks.ru";
+// export const BASE_URL = "http://localhost:3000";
+export const BASE_URL = "https://api.fox.nomoredomainsrocks.ru";
 
 const checkResponse = (res) => {
   return res.ok ? res.json() : Promise.reject(`Ошибка: ${res.status}`);
