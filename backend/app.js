70e3acf7ea5031a89fbe0db5c3d337b149ccdfb7
Alex Shevelkov <sasha.shevelkov0@yandex.ru>
2023-10-26 19:51:52 +0300

    fix: 3000 port

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,7 @@ const auth = require('./middlewares/auth');
 const { NotFoundError } = require('./errors/errors');
 const { handleError } = require('./middlewares/handleError');
 
-const { PORT = 3001, MONGO_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
+const { PORT = 3000, MONGO_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 
 const app = express();
 
